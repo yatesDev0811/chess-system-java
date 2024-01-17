bd0d624dfb0bea09f9d92add70b47b6e2c065be7
Marcelo Stefen <marcelostefen171@gmail.com>
2024-01-17 12:47:39 -0300

    "First class position"

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("salve");
-		
+		Position position = new Position(3, 5);
+		System.out.println(position);
 	}
 
 }
